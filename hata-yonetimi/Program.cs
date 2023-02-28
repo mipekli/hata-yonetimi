94c4c0b8a7925c3bd631fbc3960c2376f216b3bf
Muhammed Ipekliogullari <muhammednecati20@gmail.com>
2023-02-28 03:43:18 +0300

    Add project files.

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,45 @@
+﻿//try
+//{
+//    Console.WriteLine("bir sayı griiniz:");
+//    int sayi = Convert.ToInt32(Console.ReadLine());
+ //   Console.WriteLine("Girmiş olduğunuz sayi :" + sayi);
+ //
+//}
+//catch(Exception ex)
+//{
+ //   Console.WriteLine("Hata:"+ex.Message.ToString());
+
+///finally
+//{
+// Console.WriteLine("İşlem Tamamlandı.");
+
+//}
+
+try
+{
+    //int a = int.Parse(null);
+    //int a = int.Parse("test");
+    int a = int.Parse("-20000000000");
+}
+catch(ArgumentNullException ex)
+{
+    Console.WriteLine("Boş Değer Girdiniz!");
+    Console.WriteLine(ex);
+}
+
+catch (FormatException ex)
+{
+    Console.WriteLine("Veri tipi uygun değil!");
+    Console.WriteLine(ex);
+}
+catch(OverflowException ex)
+{
+    Console.WriteLine("Çok Küçük ya da çok büyük bir değer girdiniz");
+    Console.WriteLine(ex);
+
+}
+finally
+{
+    Console.WriteLine("hata yönetimi tamamlandı");
+
+}
